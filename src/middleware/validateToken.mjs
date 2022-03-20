84db1fa4bb1fd447bd55cb06c11ae489524380df
Plsr <christianpoplawski@gmail.com>
2022-03-20 19:46:25 +0100

    Remove console.log

--- src/middleware/validateToken.mjs
+++ src/middleware/validateToken.mjs
@@ -2,7 +2,6 @@ import adminApp from '../util/firebase.mjs'
 const SKIP_PATHNAMES = []
 
 const validateToken = async (req, res, next) => {
-  console.log(req)
   const token = req.token
   try {
     const verificationRes = await adminApp.auth().verifyIdToken(token)
