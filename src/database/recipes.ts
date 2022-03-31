dc4a6c498e9886af89d27ccdda23d2dd31e54459
Plsr <christianpoplawski@gmail.com>
2022-03-31 21:11:47 +0200

    Remove unused import

--- src/database/recipes.ts
+++ src/database/recipes.ts
@@ -1,7 +1,6 @@
 import admin from 'firebase-admin'
 import Recipe from "./schemas/recipeSchema.js";
 import { Recipe as RecipeInterface, RequestRecipe } from '../types/reicpe'
-import { assert } from 'console';
 
 export async function addRecipe(recipe: RequestRecipe, userIdToken: admin.auth.DecodedIdToken): Promise<RecipeInterface> {
   try {
